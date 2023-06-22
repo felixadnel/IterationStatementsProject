ad77cd2bea2a28c2e58a829457e081adb1cec3ad
felix adnel <flixadnel@gmail.com>
2023-06-22 02:19:05 -0500

    fixed endless loop

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Security.Cryptography;
 
 namespace IterationStatements
 {
@@ -8,85 +7,62 @@ namespace IterationStatements
     {
         static void Main(string[] args)
         {
-                //TODO - Read each comment and complete its instruction
-                // like the example below
+            // TODO - Read each comment and complete its instruction
+            // like the example below
 
-                //Create a List called "numbers" - DONE!
-                var numbers = new List<int>();
+            // Create a List called "numbers" - DONE!
+            var numbers = new List<int>();
 
             //-----START HERE------------------------------------------
             //Create a variable of type int and name it num
             //initialize the variable with a value of 0
-            
-                var myNumber = 0;
-
+            var num = 0;
 
             // Create a do-while loop and use the template below:
-
-
             // Increment num by 1
-
             // Then add num to the collection - numbers
             // Hint: reference num inside of the Add method's parentheses
             do
             {
-                myNumber++;
-
-                numbers.Add(myNumber);
-
-            } while (true);
-
-            
-              
-                
-                
-             // <---- While your variable is less than 100
-
-
+                num++;
+                numbers.Add(num);
+            } while (num < 100); // <---- While your variable is less than 100
 
             // Create a while loop
             // <--- While num is less than 200
+            while (num < 200)
             {
                 // Increment num by 1
                 // Then add num to the collection numbers
-                //HINT: copy how this was done in the do while loop
-                while (myNumber < 200);
-                myNumber ++;
-                numbers.Add(myNumber);
-
-
-
-
+                //HINT: copy how this was done in the do-while loop
+                num++;
+                numbers.Add(num);
             }
-
 
             // This is to show the user that the numbers will start increasing on the console
             Console.WriteLine("Increase:");
 
-
             // Create a foreach loop using the collection - numbers
-            //In the scope of the foreach loop, print each number in numbers
-            foreach (var item in numbers) 
-                Console.WriteLine(item);
-
-
-                Console.WriteLine("");
-            Console.WriteLine("Decrease:");
-
-
-            // Create a for loop - this will print the numbers in reverse order - from 200 to 1
-            // in your initializer set the value of i to 199
-            // in your conditional, as long as i is less than or equal to the amount of items in "numbers" - use (numbers.Count)
-            // AND as long as i is greater than or equal to 0
-            // Decrement i by 1
-            for (int i = 199; i <= numbers.Count && i >= 0; i--)  
-            //start for loop here
+            // In the scope of the foreach loop, print each number in numbers
+            foreach (var item in numbers)
             {
-                // place numbers[i] inside of the Console.WriteLine() method
-                Console.WriteLine($"{numbers [i]}");
+                Console.WriteLine(item);
             }
 
-            //------------End of exercise
+            Console.WriteLine("");
+            Console.WriteLine("Decrease:");
+
+            // Create a for loop - this will print the numbers in reverse order - from 200 to 1
+            // In your initializer, set the value of i to the last index of the numbers list (numbers.Count - 1)
+            // In your conditional, as long as i is greater than or equal to 0
+            // Decrement i by 1 in each iteration
+            for (int i = numbers.Count - 1; i >= 0; i--)
+            {
+                // Place numbers[i] inside the Console.WriteLine() method
+                Console.WriteLine(numbers[i]);
+            }
+
+            //------------End of exercise------------
         }
     }
 }
